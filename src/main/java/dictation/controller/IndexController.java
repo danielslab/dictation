4f6e08a80888ab54211a2b777682a7861b535e33
taka <takahiro.shigemoto@gmail.com>
2017-10-09 14:23:07 +0900

    Issue #4
    Twitterログインの実装。まだ途中。

--- src/main/java/dictation/controller/IndexController.java
+++ src/main/java/dictation/controller/IndexController.java
@@ -1,17 +1,61 @@
 package dictation.controller;
 
-import java.io.IOException;
+import java.net.URI;
+import java.util.HashMap;
 
+import org.springframework.social.oauth1.AuthorizedRequestToken;
+import org.springframework.social.oauth1.OAuth1Operations;
+import org.springframework.social.oauth1.OAuth1Parameters;
+import org.springframework.social.oauth1.OAuthToken;
+import org.springframework.social.twitter.api.Twitter;
+import org.springframework.social.twitter.api.impl.TwitterTemplate;
+import org.springframework.social.twitter.connect.TwitterConnectionFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @Controller
 public class IndexController {
 
-    @GetMapping("/")
-    public String questions(Model model) throws IOException {
-    	// TODO
-        return "redirect:/questions/0";
-    }
+	private static final String SPECIAL_ACCOUNT = "taka_2";
+
+	@RequestMapping(value = "/twitterlogin", method = RequestMethod.POST)
+	public ModelAndView twitterlogin(UriComponentsBuilder builder) {
+		TwitterConnectionFactory connectionFactory = new TwitterConnectionFactory(System.getenv("CONSUMER_KEY"), System.getenv("CONSUMER_SECRET"));
+		OAuth1Operations oauthOperations = connectionFactory.getOAuthOperations();
+		URI callbackURL = builder.path("/twittercallback").build().toUri();
+		OAuthToken requestToken = oauthOperations.fetchRequestToken(callbackURL.toString(), null);
+		String authorizeUrl = oauthOperations.buildAuthorizeUrl(requestToken.getValue(), OAuth1Parameters.NONE);
+		return new ModelAndView("redirect:" + authorizeUrl, new HashMap<String, Object>());
+	}
+
+	@RequestMapping(value = "/twittercallback", method = RequestMethod.GET)
+	public ModelAndView twittercallback(@RequestParam(value = "oauth_token", required = false) String oauth_token, @RequestParam(value = "oauth_verifier", required = false) String oauth_verifier) {
+		if (oauth_token == null) {
+			return new ModelAndView("redirect:/cancel", new HashMap<String, Object>());
+		}
+
+		TwitterConnectionFactory connectionFactory = new TwitterConnectionFactory(System.getenv("CONSUMER_KEY"), System.getenv("CONSUMER_SECRET"));
+		OAuth1Operations oauthOperations = connectionFactory.getOAuthOperations();
+		OAuthToken requestToken = new OAuthToken(oauth_token, oauth_verifier);
+		AuthorizedRequestToken authorizedRequestToken = new AuthorizedRequestToken(requestToken, oauth_verifier);
+		OAuthToken accessToken = oauthOperations.exchangeForAccessToken(authorizedRequestToken, null);
+
+		Twitter twitter = new TwitterTemplate(System.getenv("CONSUMER_KEY"), System.getenv("CONSUMER_SECRET"), accessToken.getValue(), accessToken.getSecret());
+		String screenName = twitter.userOperations().getUserProfile().getScreenName();
+		if (!SPECIAL_ACCOUNT.equals(screenName)) {
+			return new ModelAndView("redirect:/cancel", new HashMap<String, Object>());
+		}
+
+		return new ModelAndView("redirect:/questions", new HashMap<String, Object>());
+	}
+
+	@RequestMapping(value = "/cancel", method = RequestMethod.GET)
+	public String cancel() {
+		return "redirect:/error";
+	}
+
 }
