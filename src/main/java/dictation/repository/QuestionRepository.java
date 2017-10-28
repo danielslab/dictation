e4dda725883bba8c5162362a7c67cad183d8fbd8
taka <takahiro.shigemoto@gmail.com>
2017-10-28 14:50:50 +0900

    Issue #33

--- src/main/java/dictation/repository/QuestionRepository.java
+++ src/main/java/dictation/repository/QuestionRepository.java
@@ -1,9 +1,12 @@
 package dictation.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import dictation.model.Question;
 
 public interface QuestionRepository extends JpaRepository<Question, Long> {
+	public List<Question> findAllByOrderByIdAsc();
 	public Question findById(Long id);
 }
